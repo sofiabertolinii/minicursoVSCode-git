d465280d23e270e0067bf4403070b54750edf371
Pedro Henrique Ribeiro <pedro.henrique.lorenzoni.ribeiro@gmail.com>
2025-04-17 14:23:19 -0300

    criei o programa

---
+++ atividade avaliativa/pedro-henrique.ribeiro/pedrohenriqueribeiro.py
@@ -0,0 +1,10 @@
+n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+
+print(n1 + n2)
+print(n1 - n2)
+print((n1 + n2)/2)
+if n1 >= n2:
+    print(n1 % n2)
+else:
+    print(n2 % n1)
